7415639b7ed831f59867dcebe72acf60af453c46
Hugh Muraya <hughmuraya@gmail.com>
2022-12-01 09:35:55 +0000

    updated 4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def discovr():
-    name = dir(hidden_4)
-    for i in name:
-        if i[:2] != '__':
-            print("{:s}".format(i))
-
 if __name__ == "__main__":
-    discovr()
+    import hidden_4
+    defined = []
+    for w in dir(hidden_4):
+        if not w.startswith('__'):
+            defined.append(w)
+    defined.sort()
+    for w in defined:
+        print("{:s}".format(w))
